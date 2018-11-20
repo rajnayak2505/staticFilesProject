4eacb17f7e56bd42a9f777bba4674b30da0ade37
Rajesh Nayak <rajnayak2505@gmail.com>
2018-11-20 14:36:49 +0530

    second commit

--- staticFiles/settings.py
+++ staticFiles/settings.py
@@ -14,8 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+TEMPLATE_DIR=os.path.join(BASE_DIR,'templates')
+STATIC_DIR=os.path.join(BASE_DIR,'static')
+# print(STATIC_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -120,6 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILSES_DIRS = [
-STATIC_DIR,
-]
+STATICFILES_DIRS=["C:\Users\Rajesh\Desktop\workspace\django_projects\staticFiles\static",]
